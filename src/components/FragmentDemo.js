aee69e0f14802aa2a10a867da543f74a195ebcd3
Priyesha Yadav <priyeshayadav9192@gmail.com>
2020-02-28 22:39:14 +0530

    React fragment added

---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FragmentDemo() {
+    return (
+        <React.Fragment>
+            <h2>Heading</h2>
+            <p>Description about heading</p>
+        </React.Fragment>
+    )
+}
+
+export default FragmentDemo;
